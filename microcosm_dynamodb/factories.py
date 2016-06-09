cc2c8f19e6f475c3c4a30cbb17d74e593cfd4dc4
Adam Ever Hadani <adamhadani@gmail.com>
2016-06-09 13:57:38 -0700

    re-introduced AWS_DEFAULT_REGION environ default, after CI failures suggesting we need it

--- microcosm_dynamodb/factories.py
+++ microcosm_dynamodb/factories.py
@@ -2,13 +2,15 @@
 Factory that configures flywheel DynamoDB ORM-like framework.
 
 """
+from os import environ
+
 from flywheel import Engine
 
 from microcosm.api import defaults
 
 
 @defaults(
-    region=None,
+    region=environ.get("AWS_DEFAULT_REGION"),
 )
 def configure_flywheel_engine(graph):
     """
@@ -17,7 +19,7 @@ def configure_flywheel_engine(graph):
     """
     namespace = ()
     if graph.metadata.testing:
-        namespace = 'test'
+        namespace = "test"
 
     engine = Engine(namespace=namespace)
     engine.connect_to_region(graph.config.dynamodb.region)
