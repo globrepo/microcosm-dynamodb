3ba3a80d774c25876137ed812f438d4e79cf46fa
Jesse Myers <jesse.myers@globality.com>
2016-12-27 13:11:01 -0800

    Temporarily add PyCrypto dependency until migrated to cryptography

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
     install_requires=[
         "boto3>=1.4.0",
         "credstash>=1.11.0",
+        # XXX credstash changed to depend on cryptography, not PyCrypto; we haven't migrated yet
+        "pycrypto>=2.6.1",
         "flywheel>=0.5.0",
         "microcosm>=0.12.0",
         "microcosm-logging>-0.9.3",
